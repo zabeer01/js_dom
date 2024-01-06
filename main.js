ef2f3d75ac088f6b19421afc7f144be5abfdc8cf
ibnejabir001 <ibnejabir001@gmail.com>
2024-01-06 11:49:58 +0600

    test_04

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 const test = document.getElementById('test_01');
 test.innerHTML = 'test_seccessful';
+
+const test_02 = document.querySelectorAll('p');
+for(i=0; i<test_02.length; i++){
+    test_02[i].style.color = '#e31010'; 
+}
+
+const test_03 = document.querySelector('a');
+test_03.innerHTML = 'php with laravel';
+
+// the major difference between 
+const test_04 = document.querySelector('title');
+test_04.innerHTML = 'php with laravel';
