a1b47c37a350b862813f9647d798951bee36e7ca
ibnejabir001 <ibnejabir001@gmail.com>
2024-01-15 10:02:16 +0600

    milesStone_name,Modules_name

--- project_1/js/main.js
+++ project_1/js/main.js
@@ -1,7 +1,36 @@
-const milestonsData = JASON.parse(data).data;
+const milestonsData = JSON.parse(data).data;
 
 //load course milestone data
 
 function loadmilestons(){
     const milestones = document.querySelector('.milestones');
+   /*  milestones.innerHTML = milestonsData.;  */
+    milestones.innerHTML = `${milestonsData.map(function(milestone){
+        return `<div class="milestone border-b">
+        <div class="flex">
+          <div class="checkbox"><input type="checkbox" /></div>
+          <div>
+            <p>
+              ${milestone.name}
+              <span><i class="fas fa-chevron-down"></i></span>
+            </p>
+          </div>
+        </div>
+        <div class="hidden_panel">
+         ${milestone.modules.map(function(module){
+          return `<div class="module border-b">
+          <p>${module.name}</p>
+        </div>`
+         })}
+        </div>
+      </div>`
+    }).join("")}`; 
 }
+ 
+loadmilestons();
+
+
+/* 
+
+ 
+*/
